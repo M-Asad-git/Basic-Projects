ae2b3dacdb2065e8be2c86094492cef2bc882dab
M-Asad-git <asadpgc125@gmail.com>
2025-05-11 16:35:52 +0500

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<iostream>
+int main()
+{int age;
+    printf("enter your age:");
+scanf("%d", &age);
+if (age>=17){
+printf("adult\n");}
+else{ printf("nor an adult\n");
+}
+system("pause");
+return 0;}
