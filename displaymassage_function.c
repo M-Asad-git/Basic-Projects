0c0b0336c654b406f267595f2ee2a1b3f353b0fa
M-Asad-git <asadpgc125@gmail.com>
2025-06-13 22:03:57 +0500

    Add files via upload

---
+++ displaymassage_function.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void displaymassage();//function declaration
+
+
+int main(){
+displaymassage();// function call
+    return 0;
+}
+
+void displaymassage(){
+    printf("Welcome to c++ programming!\n");//function defination
+}
